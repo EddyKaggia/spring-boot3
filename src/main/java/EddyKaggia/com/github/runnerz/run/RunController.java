52787a028d82b2ab8a59cb55633576fd2baf2f7e
EddyKaggia <kagzcodes@gmail.com>
2024-04-08 01:10:23 -0700

    feat: added a repository and a GET endpoint

--- src/main/java/EddyKaggia/com/github/runnerz/run/RunController.java
+++ src/main/java/EddyKaggia/com/github/runnerz/run/RunController.java
@@ -1,4 +1,24 @@
 package EddyKaggia.com.github.runnerz.run;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+// Annotation says this is a REST controller -> expects JSON response
+// This is a class that responds to requests
+@RestController
 public class RunController {
+
+    private final RunRepository runRepository;
+
+    public RunController(RunRepository runRepository) {
+         this.runRepository = runRepository;
+    }
+
+    @GetMapping("/api/runs")
+    List<Run> findAll() {
+            return runRepository.findAll();
+        }
 }
